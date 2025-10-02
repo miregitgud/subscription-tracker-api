5bf69ffacaa99bf6387a3408817b6d8170664cf6
miregitgud <ayyash.jibril@gmail.com>
2025-10-02 17:46:44 +0700

    added middleware for auth

---
+++ controllers/user.controller.js
@@ -0,0 +1,35 @@
+import User from "../models/user.model.js";
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find().select("-password");
+
+    res.status(200).json({
+      success: true,
+      count: users.length,
+      data: users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const users = await User.findById(req.params.id).select("-password");
+
+    if (!users) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      success: true,
+      count: users.length,
+      data: users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import Router from "express";
+import { getAllUsers, getUser } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => res.send({ title: "GET all users" }));
+userRouter.get("/", getAllUsers);
 
-userRouter.get("/:id", (req, res) => res.send({ title: "GET user users" }));
+userRouter.get("/:id", authorize, getUser);
 
 userRouter.post("/", (req, res) => res.send({ title: "CREATE new user" }));
 
